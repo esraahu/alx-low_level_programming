ad690be78f935d16abf278cdf477546b3fff01fb
esraa <esraad98d@gmail.com>
2023-09-07 13:12:15 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
 int main(void)
 {
+\**
+ * main -Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * return: Always
+ *\ 
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-	   }			
+	return (0)
+}			
