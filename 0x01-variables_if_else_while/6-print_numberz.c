4cff80829c6f7ae8d82f530b31a1d8ee6ff54b10
esraa <esraad98d@gmail.com>
2023-09-08 12:43:17 -0700

    commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers from 0 to 9.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
+
