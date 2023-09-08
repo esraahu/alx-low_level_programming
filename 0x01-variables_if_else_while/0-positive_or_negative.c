7ac44bf256b6b8bcc07da7e47ad4e4411a46715d
esraa <esraad98d@gmail.com>
2023-09-08 11:11:27 -0700

    commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,11 +6,8 @@
  *
  * discription: true or false validation
  *
-<<<<<<< HEAD
  * Return: Always 0 (success)
-=======
  * Return : 0 (success)
->>>>>>> 275c778576484c20b3ea021ee5640f771a4021f2
  */
 int main(void)
 {
