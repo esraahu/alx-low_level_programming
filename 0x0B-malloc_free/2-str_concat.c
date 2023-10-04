d6d69694d4204c6032a0bcc53dd64fb837bd9821
esraa <esraad98d@gmail.com>
2023-10-04 09:27:31 -0700

    commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,21 +14,25 @@ char *str_concat(char *s1, char *s2)
 	int x, cx;
 
 	if (s1 == NULL)
+
 		s1 = "";
 
 	if (s2 == NULL)
+
 		s2 = "";
+
 		x = cx = 0;
 
 	while (s1[x] != '\0')
 		x++;
 
-	while (s2[y] != '\0')
+	while (s2[cx] != '\0')
 		cx++;
 
 	con = malloc(sizeof(char) * (x + cx + 1));
 
 	if (con == NULL)
+
 		return (NULL);
 
 	x = cx = 0;
