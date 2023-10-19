27eaef33a92a9aefb4edd8f4ec7ecf3690595d14
esraa <esraad98d@gmail.com>
2023-10-19 10:16:37 -0700

    commit

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,20 +11,20 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-list_t *add;
+list_t *new;
 unsigned int len = 0;
 
 while (str[len])
 len++;
 
-add = malloc(sizeof(list_t));
-if (!add)
+new = malloc(sizeof(list_t));
+if (!new)
 return (NULL);
 
-add->str = strdup(str);
-add->len = len;
-add->next = (*head);
-(*head) = add;
+new->str = strdup(str);
+new->len = len;
+new->next = (*head);
+(*head) = new;
 
 return (*head);
 }
